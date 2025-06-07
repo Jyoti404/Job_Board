5fc439847c669cd92772cc0850bf22f736838fb6
Jyoti404 <guddanmaurya2004@gmail.com>
2025-06-07 20:11:49 +0530

    extension error resolved

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -6,10 +6,10 @@ import { Button } from './ui/button'
 import { Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Label } from './ui/label'
-import AppliedJobTable from './AppliedJobTable'
-import UpdateProfileDialog from './UpdateProfileDialog'
+import AppliedJobTable from './AppliedJobTable.jsx'
+import UpdateProfileDialog from './UpdateProfileDialog.jsx'
 import { useSelector } from 'react-redux'
-import useGetAppliedJobs from '@/hooks/useGetAppliedJobs'
+import useGetAppliedJobs from '@/hooks/useGetAppliedJobs.jsx'
 
 const isResume = true;
 
